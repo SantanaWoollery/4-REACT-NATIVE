8851f740be21fc8ba5789afb8a2ed23d911384ff
Santana Woollery <santanawoollery@gmail.com>
2021-06-22 10:40:28 -0400

    Navigation Icons and Custom Side Drawer

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -11,7 +11,19 @@ class Contact extends Component {
     return (
       <ScrollView>
         <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
-          <Text>Nucamp Way</Text>
+          <Text>
+          1 Nucamp Way 
+          {"\n"} 
+          Seattle, WA 98001
+          </Text>
+          <Text style={{ marginBottom: 10 }}>
+          U.S.A.
+          </Text>
+          <Text>
+          Phone: 1-206-555-1234
+          {"\n"}
+          Email: campsites@nucamp.co
+          </Text>
         </Card>
       </ScrollView>
     );
